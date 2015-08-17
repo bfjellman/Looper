2382fc9d282275bd1f5ba36157fe81db15c7fa79
bfjellman <fjellman@uw.edu>
2015-08-17 10:55:45 -0700

    Code cleanup - Removed all overriden menu methods.

--- app/src/main/java/org/mckayscience/looper/LoadActivity.java
+++ app/src/main/java/org/mckayscience/looper/LoadActivity.java
@@ -287,26 +287,4 @@ public class LoadActivity extends Activity {
         //return path for new track
         return Environment.getExternalStorageDirectory()+ "/AndroidLooper/" + userId + "/" + songName + track + ".mp4";
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_load, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
