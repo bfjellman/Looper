fd82918e5e6e5f3129949462466c559a46909bd1
bfjellman <fjellman@uw.edu>
2015-08-17 15:07:29 -0700

    Fixed all Views for landscape orientation when a user turns the phone sideways.
    Added landscape view for Main Menu, added scroll view for Looper Activity.

--- app/src/main/java/org/mckayscience/looper/MainMenuActivity.java
+++ app/src/main/java/org/mckayscience/looper/MainMenuActivity.java
@@ -14,6 +14,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -51,6 +52,9 @@ public class MainMenuActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        //start scroll view at top
+        final ScrollView scrollview = ((ScrollView) findViewById(R.id.main_menu_scroll));
+        scrollview.requestFocus();
 
         sharedPreferences = getSharedPreferences(
                 getString(R.string.SHARED_PREFS), Context.MODE_PRIVATE);
